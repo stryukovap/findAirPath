14353f9bacba46a1e18689e2554b16079db7cf3e
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-10-22 17:44:30 +0300

    add heroku

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.use(express.static(__dirname + "/dist"));
 app.get(/.*/, function(req, res) {
   res.sendfile(__dirname + "/dist/index.html");
 });
+
 app.listen(port);
 
 window.console.log("Server started...");
